ead04f81a67f566640e2e1a7bdc12d6e1bd1cad4
zeeshan-kodex <ahsansahi6@gmail.com>
2026-07-07 12:32:45 +0500

    render html in express

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ import express from "express";
 import home from "./pages/home.js";
 import login from "./pages/login.js";
 import dataSubmit from "./pages/dataSubmit.js";
+import path from "path";
 const app = express();
-app.get("", (req, res) => {
-  res.send(home());
+// app.get("", (req, res) => {
+//   res.send(home());
+// });
+// app.get("/login", (req, res) => {
+//   res.send(login());
+// });
+// app.post("/submit", (req, res) => {
+//   res.send(dataSubmit());
+// });
+app.get("/", (req, res) => {
+  const absPath = path.resolve("view/home.html");
+  res.sendFile(absPath);
 });
 app.get("/login", (req, res) => {
-  res.send(login());
+  const absPath = path.resolve("view/login.html");
+  res.sendFile(absPath);
 });
 app.post("/submit", (req, res) => {
-  res.send(dataSubmit());
+  const absPath = path.resolve("view/dataSubmit.html");
+  res.sendFile(absPath);
 });
-
 app.listen(3000);
