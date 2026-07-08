0df1702476cef085316ef5f6813859809f648159
zeeshan-kodex <ahsansahi6@gmail.com>
2026-07-08 10:51:12 +0500

    route middleware

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 import express from "express";
 const app = express();
-function checkAge(req, res, next) {
+function checkAgeRouteMiddleware(req, res, next) {
   if (!req.query.age || req.query.age < 18) {
     res.send("You are not allowed to access this page");
   }
   next();
 }
-app.use(checkAge);
+
 app.get("/", (req, res) => {
   res.send("Home Page");
 });
 app.get("/login", (req, res) => {
   res.send("Login Page");
 });
-app.get("/admin", (req, res) => {
-  res.send("Admin Page");
+app.get("/users", checkAgeRouteMiddleware, (req, res) => {
+  res.send("Users Page");
+});
+app.get("/products", checkAgeRouteMiddleware, (req, res) => {
+  res.send("Products Page");
 });
 app.listen(3000);
