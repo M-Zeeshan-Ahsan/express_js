17a0d97dca4ff57d86f211f08fab99260081eda2
zeeshan-kodex <ahsansahi6@gmail.com>
2026-07-08 10:26:48 +0500

    basic middleware

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
-// const express = require("express");
 import express from "express";
-import home from "./pages/home.js";
-import login from "./pages/login.js";
-import dataSubmit from "./pages/dataSubmit.js";
-import path from "path";
 const app = express();
-// app.get("", (req, res) => {
-//   res.send(home());
-// });
-// app.get("/login", (req, res) => {
-//   res.send(login());
-// });
-// app.post("/submit", (req, res) => {
-//   res.send(dataSubmit());
-// });
+
+app.use((req, res, next) => {
+  console.log(req.url);
+  next();
+});
+
 app.get("/", (req, res) => {
-  const absPath = path.resolve("view/home.html");
-  res.sendFile(absPath);
-});
-app.get("/login", (req, res) => {
-  const absPath = path.resolve("view/login.html");
-  res.sendFile(absPath);
-});
-app.post("/submit", (req, res) => {
-  const absPath = path.resolve("view/dataSubmit.html");
-  res.sendFile(absPath);
-});
-app.use("", (req, res) => {
-  const absPath = path.resolve("view/404.html");
-  res.status(404).sendFile(absPath);
+  res.send("Hello World");
 });
 app.listen(3000);
