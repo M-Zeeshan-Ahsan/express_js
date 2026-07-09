faa1321086c203933189bd46deb1086e57d65859
zeeshan-kodex <ahsansahi6@gmail.com>
2026-07-09 13:07:43 +0500

    ejs

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from "express";
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.get("", (req, res) => {
-  res.render("home", { name: "zeeshan", age: "30" });
+  res.render("home");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.post("/submit", (req, res) => {
+  res.render("dataDisplay", req.body);
 });
 app.listen(3200);
