478db499b7b47fdd0f5062d1f9f2e0bd005007c2
zeeshan-kodex <ahsansahi6@gmail.com>
2026-07-09 11:14:50 +0500

    ejs template

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
 import express from "express";
-import morgan from "morgan";
-
 const app = express();
-
-// Morgan middleware
-app.use(morgan("dev"));
-
-app.get("/", (req, res) => {
-  res.send("Home Page");
+app.set("view engine", "ejs");
+app.get("", (req, res) => {
+  res.render("home", { name: "zeeshan", age: "30" });
 });
-
-app.get("/about", (req, res) => {
-  res.send1("About Page");
-});
-function errorHandler(err, req, res, next) {
-  res.status(err.status || 500).send("Something went wrong!");
-}
-app.use(errorHandler);
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-  console.log("Server running on port 3000");
-});
+app.listen(3200);
