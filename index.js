f53a274cb88a725382cc76de5bca0b3d8d298b72
zeeshan-kodex <ahsansahi6@gmail.com>
2026-07-10 16:16:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const dbUrl = "mongodb://127.0.0.1:27017";
 const client = new MongoClient(dbUrl);
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 client.connect().then((connection) => {
   const db = connection.db(dbName);
@@ -18,6 +19,19 @@ client.connect().then((connection) => {
     const collection = db.collection("students");
     const students = await collection.find().toArray();
     res.render("student", { students });
+  });
+  app.get("/add", (req, res) => {
+    res.send(`<form action="/add-student" method="post">
+      <input type="text" name="name" placeholder="Name">
+      <input type="email" name="email" placeholder="Email">
+      <input type="number" name="age" placeholder="Age">
+      <button type="submit">Add Student</button>
+    </form>`);
+  });
+  app.post("/add-student", (req, res) => {
+    const collection = db.collection("students");
+    const result = collection.insertOne(req.body);
+    res.send("data");
   });
 });
 
