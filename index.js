dcd51f6d7a93b92fb409a15f7a213cef205c185e
zeeshan-kodex <ahsansahi6@gmail.com>
2026-07-10 12:41:34 +0500

    updated

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 import express from "express";
 import { MongoClient } from "mongodb";
-const app = express();
 
 const dbName = "school";
 const dbUrl = "mongodb://127.0.0.1:27017";
 const client = new MongoClient(dbUrl);
 
-async function connectToDatabase() {
+const app = express();
+app.set("view engine", "ejs");
+app.get("/", async (req, res) => {
   await client.connect();
   const db = client.db(dbName);
   const collection = db.collection("students");
-  const result = await collection.find().toArray();
-  console.log("Connected to the database and retrieved data:", result);
-}
-connectToDatabase();
+  const students = await collection.find().toArray();
+  console.log("Connected to the database and retrieved data:", students);
+  res.render("student", { students });
+});
 app.listen(3000);
