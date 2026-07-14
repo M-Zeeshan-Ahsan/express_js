6e7d750d1b2db28c2be0cd909f0f006eab348921
zeeshan-kodex <ahsansahi6@gmail.com>
2026-07-14 15:25:53 +0500

    file uploaded

--- index.js
+++ index.js
@@ -1,68 +1,27 @@
-import mongoose, { Schema } from "mongoose";
 import express from "express";
-import studentModel from "./model/studentModel.js";
+import multer from "multer";
+
 const app = express();
-app.use(express.json());
-await mongoose.connect("mongodb://127.0.0.1:27017/school").then(() => {});
-
-app.get("/", async (req, res) => {
-  const studentData = await studentModel.find();
-  res.send(studentData);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
+const upload = multer({ storage });
 
-app.post("/save", async (req, res) => {
-  const { name, email, age } = req.body;
-  if (!req.body || !name || !age || !email) {
-    res.send({
-      message: "not save data",
-      success: false,
-      storedData: null,
-    });
-  }
-  const saveData = await studentModel.create(req.body);
+app.get("/", (req, res) => {
+  res.send(`<form action='/upload' method="post" enctype="multipart/form-data">
+    <input type="file" name="myfile" />
+    <button>Upload File</button>
+    </form>`);
+});
+app.post("/upload", upload.single("myfile"), (req, res) => {
   res.send({
-    message: "save data",
-    success: true,
-    storedData: saveData,
+    message: "file uploaded",
+    info: req.file,
   });
 });
-
-app.put("/update/:id", async (req, res) => {
-  if (!req.params.id) {
-    return res.send({
-      message: "Id Required",
-      success: false,
-    });
-  }
-
-  const updateData = await studentModel.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true },
-  );
-
-  res.send({
-    message: "Data Updated",
-    success: true,
-    updatedData: updateData,
-  });
-});
-
-app.delete("/delete/:id", async (req, res) => {
-  if (!req.params.id) {
-    return res.send({
-      message: "Id Required",
-      success: false,
-    });
-  }
-
-  const deleteData = await studentModel.findByIdAndDelete(req.params.id);
-
-  res.send({
-    message: "Data Deleted",
-    success: true,
-    deletedData: deleteData,
-  });
-});
-
 app.listen(3000);
--- schema/studentSchema.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-const studentSchema = mongoose.Schema({
-  name: String,
-  email: String,
-  age: Number,
-});
-export default studentSchema;
