608918ae7a4cf1bb550358d2eed0d05077412029
zeeshan-kodex <ahsansahi6@gmail.com>
2026-07-15 10:50:36 +0500

    cookies

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-import { MongoClient } from "mongodb";
+import express from "express";
+const app = express();
 
-const url =
-  "mongodb+srv://ahsansahi6_db_user:GoogleTest@cluster0.fpgjv6f.mongodb.net/?appName=Cluster0";
-const dbName = "school";
-const collectionName = "students";
-const client = new MongoClient(url);
-client.connect().then(() => {
-  console.log("database connected");
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  let cookieData = req.get("cookie");
+  cookieData = cookieData.split(";");
+  cookieData = cookieData[1];
+  cookieData = cookieData.split("=");
+  cookieData = cookieData[1];
+  console.log(cookieData);
+  res.render("home", { name: cookieData });
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.post("/profile", (req, res) => {
+  res.setHeader("Set-Cookie", "login=true");
+  res.setHeader("Set-cookie", "name=" + req.body.name);
+  res.render("profile");
 });
 
-async function dbConnection() {
-  const db = client.db(dbName);
-  const collection = db.collection(collectionName);
-  const result = await collection.find().toArray();
-  console.log(result);
-}
-dbConnection();
+app.listen(3200);
